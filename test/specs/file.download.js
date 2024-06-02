51fdc7de46e8c510cc7456bb0edb35e3c263281e
Dmitry Gorbachev <d.gorbachev@wolterskluwer.com>
2024-06-02 17:28:20 +0200

    Practice: File download

---
+++ test/specs/file.download.js
@@ -0,0 +1,26 @@
+import { expect, browser, $ } from '@wdio/globals';
+import { constants } from '../../constants.js';
+import fs from 'fs';
+
+const downloadsPath = global.downloadDir;
+const txtFileLinkSelector = '//a[contains(text(),"text.txt")]';
+
+describe('File downloading test', () => {
+    before('Clear the downloads content', async () => {
+        if (await fs.existsSync(downloadsPath)) {
+            await fs.rmSync(downloadsPath,{ recursive: true });
+        }
+        await fs.mkdirSync(downloadsPath,{ recursive: true });
+    })
+    it('File can be downloaded by a link', async () => {
+        await browser.url(constants.mainPageUrl);
+
+        await $('//a[@href="/download"]').click();
+        const fileLink = await $(txtFileLinkSelector);
+        expect(fileLink).toBeExisting();
+        await fileLink.click();
+        const filePath = `${downloadsPath}/text.txt`;
+        await browser.waitUntil(async () => fs.existsSync(filePath), {timeout: 5000});
+        await expect(await fs.existsSync(filePath)).toEqual(true);
+    })
+})
